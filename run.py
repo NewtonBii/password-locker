797b6da6ea788f05387f6c250fc914f2195cba4c
Newton <biinewton382@gmail.com>
2017-12-02 09:25:39 +0300

    Add create user functionality.

--- run.py
+++ run.py
@@ -46,30 +46,113 @@ def main():
         print('\n')
 
         if short_code == 'cu':
-            print("First Name")
-            fname = input()
+            print("Create a UserName")
+            created_user_name = input()
 
-            print("Last name")
-            lname = input()
-
-            print("Your Email")
-            email = input()
-
-            print("Enter a password")
-            user_password = input()
+            print("Select a Password")
+            created_user_password = input()
 
             print("Confirm Your Password")
             confirm_password = input()
 
-            if confirm_password != user_password:
-                print("Sorry your passwords did not match")
+            while confirm_password != created_user_password:
+                print("Sorry your passwords did not match!")
                 print("Enter a password")
-                user_password = input()
+                created_user_password = input()
                 print("Confirm Your Password")
                 confirm_password = input()
             else:
-                print(f"Congratulations {fname} {lname}! You have created your new account.")
+                print(f"Congratulations {created_user_name}! You have created your new account.")
                 print('\n')
+                print("Proceed to Log In to your Account")
+                print("Username")
+                entered_userName = input()
+                print("Your Password")
+                entered_password = input()
+
+                while entered_userName != created_user_name or entered_password != created_user_password:
+                    print("You entered a wrong username or password")
+                    print("Username")
+                    entered_userName = input()
+                    print("Your Password")
+                    entered_password = input()
+                else:
+                    print(f"Welcome: {entered_userName} to your Account")
+                    print('\n')
+
+                    print("Select an option below to continue: Enter 1, 2, 3, 4 or 5")
+                    print('\n')
+
+                while True:
+                    print("1: View Your saved credentials")
+                    print("2: Add new credentials")
+                    print("3: Remove credentials")
+                    print("4: Search credentials")
+                    print("5: Log Out")
+                    option = input()
+
+                    if option == '2':
+                        while True:
+                            print("Continue to add? y/n")
+
+                            choice = input().lower()
+                            if choice == 'y':
+                                print("Enter The Account Name")
+                                account_name = input()
+                                print("Enter a password")
+                                print(
+                                    "To generate random password enter keyword 'gp' or 'n' to create your own password")
+                                keyword = input().lower()
+                                if keyword == 'gp':
+                                    account_password = random.randint(111111, 1111111)
+                                    print(f"Account: {account_name}")
+                                    print(f"Password: {account_password}")
+                                    print('\n')
+                                elif keyword == 'n':
+                                    print("Create your password")
+                                    account_password = input()
+                                    print(f"Account: {account_name}")
+                                    print(f"Password: {account_password}")
+                                    print('\n')
+
+                                else:
+                                    print("Please enter a valid Code")
+
+                                save_new_credential(create_new_credential(
+                                    account_name, account_password))
+                            elif choice == 'n':
+                                break
+                            else:
+                                print("Please use 'y' for yes or 'n' for no!")
+                    elif option == '1':
+                        while True:
+                            print("Continue to View? y/n")
+                            choice1 = input().lower()
+                            if choice1 == 'y':
+                                print("Below is a list of all your credentials")
+                                if display_credentials():
+
+                                    for credential in display_credentials():
+                                        print(f"ACCOUNT NAME:{credential.account_name}")
+                                        print(f"PASSWORD:{credential.account_password}")
+
+                                else:
+                                    print('\n')
+                                    print("You don't seem to have any contacts yet")
+                                    print('\n')
+                            elif choice1 == 'n':
+                                break
+                            else:
+                                print("Please use y or n")
+                    elif option == '5':
+                        print("You have Successfully logged out")
+                        break
+
+                    elif option == '3':
+                        print("It works")
+
+                    else:
+                        print("Please enter a valid code")
 
         elif short_code == 'lg':
             print("WELCOME")
@@ -157,8 +240,6 @@ def main():
                             break
                         else:
                             print("Please use y or n")
-                # else:
-                #     print("Sorry you did not select a valid code")
                 elif option == '5':
                     print("You have Successfully logged out")
                     break
@@ -168,9 +249,10 @@ def main():
 
                 else:
                     print("Please enter a valid code")
-    else:
-        print("Please Enter a valid code to continue")
-        #
+        elif short_code == 'ex':
+            break
+        else:
+            print("Please Enter a valid code to continue")
 
 
 if __name__ == '__main__':
